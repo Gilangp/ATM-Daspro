4bdc63a0d2c77dd4f69b5886a61e119adf1db2f3
koctaa04 <yeftaocta04@gmail.com>
2023-09-15 20:56:00 +0700

    tambah fitur setor tunai

--- Main.java
+++ Main.java
@@ -67,5 +67,38 @@ public class Main {
         System.out.println("|   2. KPRB                   5. LAMZIZ       |");
         System.out.println("|   3. Dompet DHUAFA                          |");
         System.out.println("-----------------------------------------------");
+
+        // SETOR TUNAI
+        int saldo = 5000, masukan;
+        
+        // masukan input
+        System.out.println("Masukkan jumlah uang yang ingin di setor");
+        masukan = input.nextInt();
+
+        // konfirmasi
+        System.out.println("-----------------------------------------------");
+        System.out.println("|     Saldo yang ingin anda setor sebesar     |");
+        System.out.println("|                                             |");
+        System.out.println("               Rp.  "+ masukan + "  \t\t  ");
+        
+        System.out.println("-----------------------------------------------");
+
+        saldo += masukan; //saldo = saldo + masukan
+
+        // konfirmasi pin
+        System.out.println("Masukkan konfirmasi pin anda");
+        int pin = input.nextInt();
+
+        if (pin == 123456) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("|               SETOR BERHASIL                |");
+            System.out.println("|                 INFO SALDO                  |");
+            System.out.println("|                                             |");
+            System.out.println("     Saldo anda saat ini adalah Rp. "+ saldo +"  ");
+            System.out.println("|                                             |");
+            System.out.println("-----------------------------------------------");
+        } else {
+            System.out.println("Pin anda salah");
+        }
     }
 }
