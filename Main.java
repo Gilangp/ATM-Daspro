bd39d5e7357922a4d7b5403e4af045e9d33e6b5a
dhiyaul10 <dhiyaulul108@gmail.com>
2023-10-19 08:29:55 +0700

    mau pull

--- Main.java
+++ Main.java
@@ -1,9 +1,43 @@
 /**
  * Main
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+
+        // SETOR TUNAI
+        int saldo = 5000, masukan;
+        
+        // masukan input
+        System.out.println("Masukkan jumlah uang yang ingin di setor");
+        masukan = input.nextInt();
+
+        // konfirmasi
+        System.out.println("-----------------------------------------------");
+        System.out.println("|     Saldo yang ingin anda setor sebesar     |");
+        System.out.println("|                                             |");
+        System.out.println("               Rp.  "+ masukan + "  \t\t  ");
+        
+        System.out.println("-----------------------------------------------");
+
+        saldo += masukan; //saldo = saldo + masukan
+
+        // konfirmasi pin
+        System.out.println("Masukkan konfirmasi pin anda");
+        int pin = input.nextInt();
+
+        if (pin == 123456) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("|               SETOR BERHASIL                |");
+            System.out.println("|                 INFO SALDO                  |");
+            System.out.println("|                                             |");
+            System.out.println("     Saldo anda saat ini adalah Rp. "+ saldo +"  ");
+            System.out.println("|                                             |");
+            System.out.println("-----------------------------------------------");
+        } else {
+            System.out.println("Pin anda salah");
+        }
     }
 }
